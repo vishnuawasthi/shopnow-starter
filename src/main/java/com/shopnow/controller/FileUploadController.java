c9905166c05a12428d1f517da10e9d36268916fa
vishnuawasthi121 <vishnuawasthi121>
2016-01-27 17:15:18 +0530

    file upload utils

--- src/main/java/com/shopnow/controller/FileUploadController.java
+++ src/main/java/com/shopnow/controller/FileUploadController.java
@@ -1,5 +1,10 @@
 package com.shopnow.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,10 +14,13 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shopnow.form.FilUploadForm;
+import com.shopnow.utils.FileUploadUtils;
 
 @Controller
 public class FileUploadController {
-
+	@Autowired
+	private FileUploadUtils  fileUploadUtils ;
+	
 	@RequestMapping(value = "/file", method = RequestMethod.GET)
 	public ModelAndView file() {
 		System.out.println("file() -start");
@@ -25,14 +33,19 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
 	public ModelAndView fileUpload(@ModelAttribute("filUploadForm") FilUploadForm filUploadForm,
-			BindingResult result) {
+			BindingResult result,HttpServletRequest request) {
 		System.out.println("fileUpload() -start");
-		
 		MultipartFile file = filUploadForm.getFile();
-		
-		System.out.println("Name  : "+file.getName());
-		System.out.println("Original file name:  "+ file.getOriginalFilename());
-		
+		try {
+			System.out.println("Name  : "+file.getName());
+			System.out.println("Original file name:  "+ file.getOriginalFilename());
+			String fileName  =	fileUploadUtils.createFile(file, request,"IMAGE");
+			System.out.println("fileName   : "+fileName);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		ModelAndView modelAndView = new ModelAndView();
 		// modelAndView.addObject("filUploadForm", new FilUploadForm());
